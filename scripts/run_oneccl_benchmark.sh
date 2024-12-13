350b74b233461893d01d1ff409a21c04d3046866
Sydir, Jerry <jerry.sydir@intel.com>
2024-12-13 10:24:02 -0800

    updated run_oneccl script

--- scripts/run_oneccl_benchmark.sh
+++ scripts/run_oneccl_benchmark.sh
@@ -16,11 +16,13 @@ export DTO_WAIT_METHOD=yield
 export DTO_MIN_BYTES=8192
 export DTO_CPU_SIZE_FRACTION=0.33
 export DTO_AUTO_ADJUST_KNOBS=1
+export DTO_LOG_LEVEL=1
  
 # Use this if using DTO
 #export LD_PRELOAD=$libfabric/libfabric.so:/home/jjsydir/internal_dto/projects.research.dto_dev/libdevdto.so.1.0
 #export LD_PRELOAD=$libfabric/libfabric.so:/home/jjsydir/internal_dto/projects.research.dto_dev/libdto.so.1.0
-export LD_PRELOAD=$libfabric/libfabric.so:/home/jjsydir/old_dto/DTO/libdto.so.1.0
+#export LD_PRELOAD=$libfabric/libfabric.so:/home/jjsydir/old_dto/DTO/libdto.so.1.0
+export LD_PRELOAD=$libfabric/libfabric.so:/home/jjsydir/clean_dto/DTO/libdto.so.1.0
 # Otherwise use this to use default stdc mem* operations
 #export LD_PRELOAD=$libfabric/libfabric.so
  
@@ -38,10 +40,12 @@ export CCL_WORKER_COUNT=$workers # Set number of CCL workers, default=1
 #export CCL_LOG_LEVEL=info
  
 # Example Allreduce command line, 2 ranks on the same node. Use --inplace 1 to enable in-place reductions. Default is out of place
-$oneccl/bin/mpiexec.hydra -n $ranks $oneccl/examples/benchmark/benchmark --iter_policy off --coll allreduce -f $(( 4*1024 )) -t $(( 32*1024*1024 )) -w 50 -i 50 --check last --inplace 1
+#$oneccl/bin/mpiexec.hydra -n $ranks $oneccl/examples/benchmark/benchmark --iter_policy off --coll allreduce -f $(( 4*1024 )) -t $(( 32*1024*1024 )) -w 50 -i 50 --check last --inplace 1
 #$oneccl/bin/mpiexec.hydra -n $ranks $oneccl/examples/benchmark/benchmark --iter_policy off --coll allreduce -f $(( 4*1024 )) -t $(( 32*1024*1024 )) -w 50 -i 5000 --check all --inplace 1
 
-#while [ "$tries" -gt 0 ]; do
-#    $oneccl/bin/mpiexec.hydra -n $ranks $oneccl/examples/benchmark/benchmark --iter_policy off --coll allreduce -f $(( 4*1024 )) -t $(( 32*1024*1024 )) -w 50 -i 50 --check last --inplace 1
-#    tries=$(( tries - 1 ))
-#indone
+while [ "$tries" -gt 0 ]; do
+    #$oneccl/bin/mpiexec.hydra -n $ranks $oneccl/examples/benchmark/benchmark --iter_policy off --coll allreduce -f $(( 4*1024 )) -t $(( 32*1024*1024 )) -w 50 -i 50 --check last --inplace 1
+    $oneccl/bin/mpiexec.hydra -n $ranks $oneccl/examples/benchmark/benchmark --iter_policy off --coll allreduce -f $(( 8*1024*1024 )) -t $(( 32*1024*1024 )) -w 50 -i 50 --check all --inplace 1
+    tries=$(( tries - 1 ))
+    sleep 1
+done
