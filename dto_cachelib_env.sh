b755d9f34b7d06d7aded6a0b7f3cbdf999f8e7e2
Daniel Byrne <byrnedj12@gmail.com>
2025-05-01 10:42:05 -0700

    update compile

--- dto_cachelib_env.sh
+++ dto_cachelib_env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 export DTO_USESTDC_CALLS=0
-export DTO_COLLECT_STATS=0
-export DTO_WAIT_METHOD=sleep
+export DTO_COLLECT_STATS=1
+export DTO_WAIT_METHOD=umwait
 export DTO_MIN_BYTES=32768
 export DTO_CPU_SIZE_FRACTION=0.0
 export DTO_AUTO_ADJUST_KNOBS=0
